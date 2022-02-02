f0894fe624d3add5192f975b2f223173f9af4a05
Francisco José de Cerqueira Antunes Filho <45143279+Mendaccium@users.noreply.github.com>
2022-02-02 11:48:29 -0300

    adapter

---
+++ src/adapter/client.ts
@@ -0,0 +1,13 @@
+import { EmailValidatorAdapter } from "./validation/email-validator-adapter";
+import { EmailValidatorProtocol } from "./validation/email-validator-protocol";
+
+function validaEmail(emailValidator: EmailValidatorProtocol, email: string): void {
+    if(emailValidator.isEmail(email)){
+        console.log("Valid email");
+    } else {
+        console.log("Invalid email");
+    }
+}
+
+validaEmail(new EmailValidatorAdapter(), "antunes.f@gmail.com");
+
